59a1f388faa01a74011bfb9dd39eb28eaed74a6c
AmoMuK <edyk777@gmail.com>
2023-04-22 12:05:40 +0200

    v1

--- app.py
+++ app.py
@@ -1,81 +1,84 @@
-from flask import Flask, render_template, request
-import os
-import numpy as np
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os 
 import cv2
 import pytesseract
+import pandas as pd
+from spellchecker import SpellChecker
 
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
- 
+UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def is_allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png', 'gif'}
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['file']
-    if file:
-        img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-        # perform image processing and OCR here
-        return 'OCR results'
-    else:
-        return 'No file uploaded'
-    
-# set the path where uploaded files will be stored
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
-# define the route for the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# define the route for file upload
-@app.route('/upload', methods=['POST'])
-def upload():
-    # get the uploaded file from the form
-    file = request.files['file']
-    filename = file.filename
-    # save the uploaded file to the UPLOAD_FOLDER directory
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
-    # read the uploaded file using OpenCV
-    img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
-    # rescale the image to 300 DPI
-    img = cv2.resize(img, None, fx=300/img.shape[1], fy=300/img.shape[1], interpolation=cv2.INTER_CUBIC)
-    
-    # increase the contrast of the image
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    
-    # remove noise from the image
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-    result = 255 - opening
-    
-    # de-skew the image
-    coords = np.column_stack(np.where(result > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = img.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    result = cv2.warpAffine(result, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    
-    # set up the Tesseract OCR parameters
-    config = ('-l ron --oem 4 --psm 6')
-    
-    # read the text from the image using Tesseract OCR
-    text = pytesseract.image_to_string(result, config=config)
-    
-    # display the extracted text
-    return render_template('result.html', text=text)
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-if __name__ == '__main__':
+    file = request.files['image']
+
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+
+    if file and is_allowed_file(file.filename):
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Preprocessing
+        image = cv2.medianBlur(image, 5)
+        image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        image = cv2.morphologyEx(image, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (2,2)))
+        image = cv2.dilate(image, None, iterations=3)
+
+        # De-skew
+        coords = cv2.findNonZero(image)
+        rect = cv2.minAreaRect(coords)
+        angle = rect[-1]
+        if angle < -45:
+            angle = -(90 + angle)
+        else:
+            angle = -angle
+        (h, w) = image.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        image = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+        # OCR
+        text = pytesseract.image_to_string(image, lang='ron', config='--psm 6 --oem 4 -c tessedit_char_blacklist=АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+
+        # Spell correction
+        spell = SpellChecker(language='ro')
+        words = text.split()
+        corrected_words = [spell.correction(word) for word in words]
+        text = ' '.join(corrected_words)
+
+        # Remove empty lines
+        text = '\n'.join([line for line in text.splitlines() if line.strip()])
+
+        # Save to CSV file
+        df = pd.DataFrame({'text': [text]})
+        df.to_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename + '.csv'), index=False)
+
+        flash('Image successfully uploaded and OCR performed')
+        return redirect(url_for('home'))
+    else:
+        flash('Image successfully uploaded and OCR performed')
+        return redirect(url_for('home'))
+    
+if __name__ == "__main__":
     app.run(debug=True)
